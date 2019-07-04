601dd04992217a985e098fbe20c5728202d2254d
Vladimir <x55aah@gmail.com>
2019-07-04 18:57:38 +0300

    add rest api url

--- routes.py
+++ routes.py
@@ -1,9 +1,9 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from discovery import handlers
-from discovery.api.compat.rest import routes as rest_routes
+from .api.compat.rest import routes as rest_routes
 from anthill.framework.utils.urls import include
+from . import handlers
 
 route_patterns = [
     url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
